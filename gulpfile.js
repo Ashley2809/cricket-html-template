2707854ca025247dcd3eb92f7f2b19c83f4f44e0
arunbscmdu <arun.baskaran@roanuz.com>
2017-07-24 19:15:37 +0530

    news aggregation widget css updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
-
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var changed = require('gulp-changed');
+var connect = require('gulp-connect');
 
-gulp.task('styles', function() {
-    gulp.src('scss/*')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('css/'))
+gulp.task('connect', function() {
+  connect.server({
+    name: 'Dev App',
+    root: '',
+    livereload: true,
+    port: 8002,
+  });
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('scss/*.scss',['styles']);
+gulp.task('html', function () {
+  gulp.src('*.html')
+    .pipe(connect.reload());
 });
+
+gulp.task('css', function() {
+  gulp.src('scss/*.scss')
+  .pipe(changed('/css/'))
+  .pipe(sass().on('error', sass.logError))
+  .pipe(gulp.dest('css'));
+});
+
+
+gulp.task('watch', function () {
+  gulp.watch('scss/*.scss', ['css']);
+  gulp.watch(['*.html'], ['html']); 
+});
+
+gulp.task('default', ['css','watch','connect','html']);
